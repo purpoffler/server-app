6c6253f79e3e8fb217371a957ef9515dfec2eeac
Dmitry Baranov <mainarchi@yandex.ru>
2025-09-27 12:54:37 +0300

    -Добавил в .gitignore файл с логами и файлы с выводом пользовательского текста -Добавил до цикл while и флаг isClientDisconnected,сервер теперь не надо перезагружать при отключении клиента, и в логи бесконечно не спамится, что клиент отключен

--- src/layer/ServerLevel.java
+++ src/layer/ServerLevel.java
@@ -12,6 +12,7 @@ import java.util.concurrent.TimeUnit;
 public class ServerLevel implements Runnable {
     private final BlockingQueue<UserPackage> inputQueue;
     private final BlockingQueue<String> resultValidateQueue;
+    private boolean isClientDisconnected = false;
 
     public ServerLevel(BlockingQueue<UserPackage> inputQueue, BlockingQueue<String> resultValidateQueue) {
         this.inputQueue = inputQueue;
@@ -19,34 +20,40 @@ public class ServerLevel implements Runnable {
     }
 
     public void run() {
-        try (Connection connection = new Connection()) {
-            ConsoleHelper.writeSystemMessage("Клиент подключился");
-            while (true) {
-                String word = connection.receive(); // ждём пока клиент что-нибудь нам напишет
-                //ConsoleHelper.writeSystemMessage(word);
-                if (!word.isEmpty()) {
-                    String[] packetBlocks = word.split("\\|");
-                    //Создаем dto UserPackage
-                    String date = DateCalculator.getDate();
-                    UserPackage userPackage = new UserPackage(packetBlocks[0], packetBlocks[1], ExpectedDataType.valueOf(packetBlocks[2].trim().toUpperCase()), packetBlocks[3], packetBlocks[4], connection.getIp(), date);
-                    //ConsoleHelper.writeMessage(connection.getIp() + " " + date);
-                    inputQueue.put(userPackage);
-                    //Забираем результат проверки покета из очереди
-                    String resultValidatePacket = resultValidateQueue.poll(500, TimeUnit.MILLISECONDS);
-                    //ConsoleHelper.writeSystemMessage("То что забралось из очереди " + resultValidatePacket);
-                    if (resultValidatePacket != null) {
-                        connection.send("Привет, это Сервер! Подтверждаю, вы написали : " + word + " " + "\u001B[32m" + resultValidatePacket + "\u001B[0m" + "\n");
+        while (true) {
+            try (Connection connection = new Connection()) {
+                ConsoleHelper.writeSystemMessage("Клиент подключился");
+                while (true) {
+                    String word = connection.receive(); // ждём пока клиент что-нибудь нам напишет
+                    isClientDisconnected = false;
+                    //ConsoleHelper.writeSystemMessage(word);
+                    if (!word.isEmpty()) {
+                        String[] packetBlocks = word.split("\\|");
+                        //Создаем dto UserPackage
+                        String date = DateCalculator.getDate();
+                        UserPackage userPackage = new UserPackage(packetBlocks[0], packetBlocks[1], ExpectedDataType.valueOf(packetBlocks[2].trim().toUpperCase()), packetBlocks[3], packetBlocks[4], connection.getIp(), date);
+
+                        inputQueue.put(userPackage);
+                        //Забираем результат проверки пакета из очереди
+                        String resultValidatePacket = resultValidateQueue.poll(500, TimeUnit.MILLISECONDS);
+                        //ConsoleHelper.writeSystemMessage("То что забралось из очереди " + resultValidatePacket);
+                        if (resultValidatePacket != null) {
+                            connection.send("Привет, это Сервер! Подтверждаю, вы написали : " + word + " " + "\u001B[32m" + resultValidatePacket + "\u001B[0m" + "\n");
+                        }
+                    } else {
+                        connection.send("false");
                     }
-                } else {
-                    connection.send("false");
+                }
+            } catch (InterruptedException e) {
+                ConsoleHelper.writeSystemMessage("Ошибка в считывании пакета");
+            } catch (ClassNotFoundException e) {
+                ConsoleHelper.writeSystemMessage("Клиент ничего не написал. Ошибка в ServerLevel в методе .receive()");
+            } catch (IOException e) {
+                if (!isClientDisconnected) {
+                    ConsoleHelper.writeSystemMessage("Клиент отключился, жду нового клиента...");
+                    isClientDisconnected = true;
                 }
             }
-        } catch (InterruptedException e) {
-            ConsoleHelper.writeSystemMessage("Ошибка в считывании пакета");
-        } catch (ClassNotFoundException e) {
-            ConsoleHelper.writeSystemMessage("Ошибка во время отправки ответа");
-        } catch (IOException e) {
-            ConsoleHelper.writeSystemMessage("Клиент отключился");
         }
     }
 }
